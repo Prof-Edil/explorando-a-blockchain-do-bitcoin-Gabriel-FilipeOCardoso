d7e0bccd01ae83613702b8aa6d5639aa9b06966f
Gabriel filipe Oliveira Cardoso <gabrie_fili@hotmail.com>
2024-12-27 11:25:10 -0300

    q123

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,7 @@
 # How many new outputs were created by block 123,456?
+# instalado jq
+hash=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblockhash 123456)
+
+json=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblock $hash | jq)
+
+echo $json | jq -r '.nTx'
