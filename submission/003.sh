deff3b174b661806b04dfdd53b8f04d512908606
Gabriel filipe Oliveira Cardoso <gabrie_fili@hotmail.com>
2025-02-01 15:26:22 -0300

    q123p1

--- submission/003.sh
+++ submission/003.sh
@@ -2,6 +2,7 @@
 # instalado jq
 hash=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblockhash 123456)
 
-json=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblock $hash | jq)
+bloco=$(bitcoin-cli -rpconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblock $hash 2)
 
-echo $json | jq -r '.nTx'
+out=$($bloco | jq '[.tx[].vout | length] | add')
+
