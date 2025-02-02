30f84a53bccd794d9ba833d6b97020a5edb1825d
Gabriel filipe Oliveira Cardoso <gabrie_fili@hotmail.com>
2025-02-02 09:18:05 -0300

    q1234p2

--- submission/004.sh
+++ submission/004.sh
@@ -1,10 +1,12 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 
-descriptor="tr([00000000/86'/0'/0']xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/*)"
+xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 
-valor=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getdescriptorinfo "$descriptor" | jq -r '.descriptor')
+descriptor="tr($xpub/100)"
 
-address=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 deriveaddresses "$valor" | jq -r '.[0]')
+descriptorc=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getdescriptorinfo "$descriptor" | jq -r '.descriptor')
+
+address=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 deriveaddresses "$descriptorc" | jq -r '.[0]')
 
 echo $address
