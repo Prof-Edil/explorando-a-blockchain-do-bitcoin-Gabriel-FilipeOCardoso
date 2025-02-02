5388afc3db682a511d5734d46588050e3c7bfe9d
Gabriel filipe Oliveira Cardoso <gabrie_fili@hotmail.com>
2025-02-01 21:17:23 -0300

    q1234

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,10 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+
+descriptor="tr([00000000/86'/0'/0']xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/100)"
+
+valor=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getdescriptorinfo "$descriptor" | jq -r '.descriptor')
+
+address=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 deriveaddresses "$valor" | jq -r '.[0]')
+
+echo $address
