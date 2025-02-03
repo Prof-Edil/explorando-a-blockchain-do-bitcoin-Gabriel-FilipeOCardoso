301d95be4cc7199c31abccec573c169dadfe55bb
Gabriel filipe Oliveira Cardoso <gabrie_fili@hotmail.com>
2025-02-02 21:22:33 -0300

    q7go

--- submission/007.sh
+++ submission/007.sh
@@ -1,29 +1,35 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
 
-blockhash=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblockhash 123321)
+#!/bin/bash
 
-bloco=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblock "$blockhash")
+# Block height to check
+block_height=123321
 
-for txid in $(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblock "$bloco" | jq -r '.tx[]')
-do
-    echo $(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblock "$bloco" | jq -r '.tx[]')
-    tx=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getrawtransaction "$txid" true)
+# Get the block hash
+block_hash=$(bitcoin-cli getblockhash $block_height)
 
-    for vout in $(echo $tx | jq -r '.vout[] | .n')
-    do
-        # Verifique se a saída foi gasta usando gettxout
-        txout=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 gettxout "$txid" "$vout")
+# Get the block details
+block=$(bitcoin-cli getblock $block_hash)
 
-        # Se gettxout retornar null, a saída foi gasta
-        if [ $txout == null ] 
-        then
-            echo "Saída $vout da transação $txid FOI GASTA."
-        else
-            echo "Saída $vout da transação $txid NÃO FOI GASTA."
-            saidatx=$(echo $vout)
-            break 2
+# Iterate over all transactions in the block
+for txid in $(echo "$block" | jq -r '.tx[]'); do
+    echo "Checking transaction: $txid"
+
+    # Get the transaction details
+    tx=$(bitcoin-cli getrawtransaction "$txid" true)
+
+    # Iterate over all outputs (vout) in the transaction
+    for vout in $(echo "$tx" | jq -r '.vout[] | .n'); do
+        # Check if the output is unspent
+        txout=$(bitcoin-cli gettxout "$txid" "$vout")
+
+        if [ "$txout" != "null" ]; then
+            # Extract the address from the unspent output
+            address=$(echo "$txout" | jq -r '.scriptPubKey.addresses[0]')
+
+            echo "Unspent output found in transaction $txid, vout $vout."
+            echo "Address: $address"
+            exit 0  # Exit after finding the unspent output
         fi
     done
 done
-
-echo $saidatx | jq -r '.vout[].scriptPubKey.addresses[]'
