43834bef40094aadd3213f80b2071721304b72a5
Gabriel filipe Oliveira Cardoso <gabrie_fili@hotmail.com>
2025-02-02 22:43:47 -0300

    q7

--- submission/007.sh
+++ submission/007.sh
@@ -3,30 +3,28 @@
 
 blockh=123321
 
-# Get the block hash
+# hash do bloco
 block_hash=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblockhash $blockh)
 
-# Get the block details
-block=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblock $block_hash)
+bloco=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getblock $block_hash)
 
-# Iterate over all transactions in the block
-for txid in $(echo "$block" | jq -r '.tx[]'); do
-    echo "Checking transaction: $txid"
+# todas as transações do bloco
+for txid in $(echo "$bloco" | jq -r '.tx[]') 
+do
 
-    # Get the raw transaction details
     tx=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 getrawtransaction "$txid" true)
+    vcont=$(echo "$tx" | jq '.vout | length')
 
-    # Iterate over all outputs (vout) in the transaction
-    for vout in $(echo "$tx" | jq -r '.vout[] | .n'); do
-        # Check if the output is unspent
-        txout=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 gettxout "$txid" "$vout")
+    
+    for i in $vcont
+    do
+        txout=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_225 -rpcpassword=V4elTiWX5gf6 gettxout "$txid" "$i")
 
-        # If txout is valid (not null), then it's an unspent output
+        # Se txout  (not null), então unspent output
         if [[ "$txout" != "null" && -n "$txout" ]]; then
-            # Extract the address from the unspent output
+            # the address
             address=$(echo "$txout" | jq -r '.scriptPubKey.addresses')
 
-            echo "Unspent output found in transaction $txid, vout $vout."
             echo "Address: $address"
             break 2  # Break both loops as we found the unspent output
         fi
